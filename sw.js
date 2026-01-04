55da30256906d8100954aaadcbc0354395276ea2
visitlanka256-sketch <visitlanka256@gmail.com>
2026-01-05 00:05:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
+const CACHE_NAME = 'sinhala-caption-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './admin.html',
+    './css/style.css',
+    './js/db.js',
+    './js/app.js',
+    './js/admin.js',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png'
+];
 
+// Service worker events for caching and offline support
