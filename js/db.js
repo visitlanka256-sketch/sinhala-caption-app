b709737591a68f94d89bf0b9ee8f4ed6e107ea4b
visitlanka256-sketch <visitlanka256@gmail.com>
2026-01-05 00:07:44 +0530

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,1 +1,17 @@
+const DB = {
+    init() {
+        // Initialize localStorage
+    },
 
+    getCaptions() {
+        // Get all captions
+    },
+
+    addCaption(caption) {
+        // Add new caption
+    },
+
+    // Other database functions...
+};
+
+// Initialize with sample data
