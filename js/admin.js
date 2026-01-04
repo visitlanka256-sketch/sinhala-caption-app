5f4c1e99ef96e19f710f4dee1b5330367fb5f438
visitlanka256-sketch <visitlanka256@gmail.com>
2026-01-05 00:07:15 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,19 @@
+let isLoggedIn = false;
 
+// Login functions
+function adminLogin(e) { /* ... */ }
+function showDashboard() { /* ... */ }
+
+// Caption management
+function addCaption(e) { /* ... */ }
+function quickAddCaption() { /* ... */ }
+function bulkAddCaptions() { /* ... */ }
+
+// Data management
+function exportData() { /* ... */ }
+function importData(event) { /* ... */ }
+function clearAllData() { /* ... */ }
+
+// Helper functions
+function getCategoryEmoji(category) { /* ... */ }
+function showAdminToast(message, type) { /* ... */ }
