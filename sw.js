956dd5f6d12a44441ef6fa015370d96b6b4aa9f1
visitlanka256-sketch <visitlanka256@gmail.com>
2026-01-05 13:14:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "sinhala-caption-v1";
-const FILES_TO_CACHE = [
+const FILES = [
   "./",
   "./index.html",
   "./css/style.css",
@@ -7,14 +7,10 @@ const FILES_TO_CACHE = [
   "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
