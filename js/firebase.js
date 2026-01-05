897173d7613b186c13aedf14b1e07aafa45c40be
visitlanka256-sketch <visitlanka256@gmail.com>
2026-01-05 19:14:21 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,27 +1,23 @@
-// js/firebase.js
 import { initializeApp } from
-  "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+"https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 
 import { getAuth } from
-  "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+"https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
 import { getDatabase } from
-  "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+"https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
-/* 🔥 YOUR FIREBASE CONFIG */
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "PASTE_HERE",
+  authDomain: "PASTE_HERE",
+  databaseURL: "PASTE_HERE",
+  projectId: "PASTE_HERE",
+  storageBucket: "PASTE_HERE",
+  messagingSenderId: "PASTE_HERE",
+  appId: "PASTE_HERE"
 };
 
-/* INIT */
 const app = initializeApp(firebaseConfig);
 
-/* EXPORTS */
 export const auth = getAuth(app);
-export const db   = getDatabase(app);
+export const db = getDatabase(app);
