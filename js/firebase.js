aa4f78559cdb7b78b9c2efc14bf4505580b3ff66
visitlanka256-sketch <visitlanka256@gmail.com>
2026-01-06 22:35:36 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAODNsLvrVieXeSVIj2Wd4o1jJO7n_sg6k",
-  authDomain: "sinhala-caption-app.firebaseapp.com",
-  databaseURL: "https://sinhala-caption-app-default-rtdb.firebaseio.com",
-  projectId: "sinhala-caption-app",
-  storageBucket: "sinhala-caption-app.firebasestorage.app",
-  messagingSenderId: "534197636566",
-  appId: "1:534197636566:web:aa8767b5b8e21eb3e66877"
+  apiKey: "AIzaSyDghETttXFNP4NPH8Sfpsf6fCvQ4NHN45o",
+  authDomain: "sinhalacaptionhub.firebaseapp.com",
+  databaseURL: "https://sinhalacaptionhub-default-rtdb.firebaseio.com",
+  projectId: "sinhalacaptionhub",
+  storageBucket: "sinhalacaptionhub.firebasestorage.app",
+  messagingSenderId: "852993684108",
+  appId: "1:852993684108:web:eab802c0685a7e9fd581a9"
 };
 
 export const app = initializeApp(firebaseConfig);
