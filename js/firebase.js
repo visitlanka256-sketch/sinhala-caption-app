b26a87a034449ea688a49007bc86d75c4e2fc391
visitlanka256-sketch <visitlanka256@gmail.com>
2026-01-07 00:58:45 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,20 @@
-// js/firebase.js (CDN version – GitHub Pages compatible)
+// js/firebase.js (GitHub Pages compatible)
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
-/* 🔥 YOUR REAL FIREBASE CONFIG */
 const firebaseConfig = {
-  apiKey: "AIzaSyDghETttXFNP4NPH8Sfpsf6fCvQ4NHN45o",
-  authDomain: "sinhalacaptionhub.firebaseapp.com",
-  databaseURL: "https://sinhalacaptionhub-default-rtdb.firebaseio.com",
-  projectId: "sinhalacaptionhub",
-  storageBucket: "sinhalacaptionhub.firebasestorage.app",
-  messagingSenderId: "852993684108",
-  appId: "1:852993684108:web:f54dc56ff462ebeed581a9"
+    apiKey: "AIzaSyABWsasIo80hJhsIcnv-S3FZqjL3wWG_8w",
+  authDomain: "sinhala-caption-app-728f9.firebaseapp.com",
+  databaseURL: "https://sinhala-caption-app-728f9-default-rtdb.firebaseio.com",
+  projectId: "sinhala-caption-app-728f9",
+  storageBucket: "sinhala-caption-app-728f9.firebasestorage.app",
+  messagingSenderId: "278795939200",
+  appId: "1:278795939200:web:d3554344be4e8453f56565",
+  measurementId: "G-M8HHTLPV3M"
 };
 
-/* ✅ INIT */
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
