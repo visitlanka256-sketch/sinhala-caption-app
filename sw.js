7314fca55dfa7a8f70f20cb132713a4dac601262
visitlanka256-sketch <visitlanka256@gmail.com>
2026-01-08 14:03:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-self.addEventListener("fetch", () => {});
+const CACHE_NAME = "sinhala-caption-v3";
+
+const STATIC_ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./js/firebase.js",
+  "./manifest.json",
+  "./icons/icon-192x192.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
